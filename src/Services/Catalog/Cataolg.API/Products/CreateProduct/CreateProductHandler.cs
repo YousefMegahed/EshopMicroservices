335e501fc4f2a62baf62c7b38b20db91b7d9b775
Yousef Megahed <yousef.megahed@amcde.com>
2024-11-03 00:12:10 +0300

    Add CQRS in building blocks class library

---
+++ src/Services/Catalog/Cataolg.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace Cataolg.API.Products.CreateProduct
+{
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+
+
+    public class CreateProductHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+
+
+            var product = new Product
+            {
+                Category = command.Category,
+                Description = command.Description,
+                Name = command.Name,
+                Price = command.Price,
+                ImageFile = command.ImageFile
+            };
+
+
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+
+            // logic here 
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
--- src/Services/Catalog/Cataolg.API/Program.cs
+++ src/Services/Catalog/Cataolg.API/Program.cs
@@ -1,6 +1,21 @@
+
 var builder = WebApplication.CreateBuilder(args);
+
+//add services to container.
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+        }
+    );
+builder.Services.AddMarten(opt => 
+  opt.Connection(builder.Configuration.GetConnectionString("Database")!)
+).UseLightweightSessions();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
