d676119a7b5cf86e5fb5ce206cf46f0e426e244e
Yousef Megahed <yousef.megahed@amcde.com>
2025-05-24 00:56:28 +0300

    Add Carter endpoints, pagination, and health checks
    Enabled modular routing with Carter and added endpoints for order creation, updates, deletion, and retrieval (by customer, name, and pagination). Introduced `PaginatedResult` and `PaginationRequest` for handling pagination. Configured health checks with SQL Server and a custom response writer. Cleaned up unused namespaces and folders. Updated project files with necessary dependencies and global `using` directives.

--- src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
+++ src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
@@ -1,12 +1,4 @@
-﻿using Ordering.Application.Dtos;
-using Ordering.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ordering.Application.Extensions
+﻿namespace Ordering.Application.Extensions
 {
     public static  class OrderExtensions
     {
